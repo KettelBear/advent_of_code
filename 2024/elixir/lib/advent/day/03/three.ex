556fbd75d60eb6f9073a3a805f4b152f27d90289
KettelBear <kettel.bear@gmail.com>
2024-12-03 14:20:14 -0500

    2024 - Day 3 - Refactor

--- 2024/elixir/lib/advent/day/03/three.ex
+++ 2024/elixir/lib/advent/day/03/three.ex
@@ -3,24 +3,24 @@ defmodule Advent.Day.Three do
 
   import Advent.Utility
 
+  @part_one_input "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
   @doc false
   def part1 do
-    input = "#{__DIR__}/input.test" |> parse_input!() |> hd()
-
     ~r/mul\(\d{1,3},\d{1,3}\)/
-    |> Regex.scan(input)
+    |> Regex.scan(@part_one_input)
     |> Enum.reduce(0, fn ["mul" <> rest], sum ->
       [n1, n2] = String.split(rest, ["(", ",", ")"], trim: true)
       sum + stoi(n1) * stoi(n2)
     end)
   end
 
+  @part_two_input "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
   @doc false
   def part2 do
-    input = "#{__DIR__}/input.test2" |> parse_input!() |> hd()
-
     ~r/mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/
-    |> Regex.scan(input)
+    |> Regex.scan(@part_two_input)
     |> Enum.reduce({0, true}, fn
       ["mul" <> rest], {sum, true} ->
         [n1, n2] = String.split(rest, ["(", ",", ")"], trim: true)
