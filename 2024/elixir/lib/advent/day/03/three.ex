c34a80f611de82f041b21f1b2c26ac2857862db3
KettelBear <kettel.bear@gmail.com>
2024-12-03 11:36:55 -0500

    2024 - Day 3 - Refactor

--- 2024/elixir/lib/advent/day/03/three.ex
+++ 2024/elixir/lib/advent/day/03/three.ex
@@ -7,9 +7,9 @@ defmodule Advent.Day.Three do
   def part1 do
     input = "#{__DIR__}/input.test" |> parse_input!() |> hd()
 
-    Regex.scan(~r/mul\(\d{1,3},\d{1,3}\)/, input)
-    |> List.flatten()
-    |> Enum.reduce(0, fn "mul" <> rest, sum ->
+    ~r/mul\(\d{1,3},\d{1,3}\)/
+    |> Regex.scan(input)
+    |> Enum.reduce(0, fn ["mul" <> rest], sum ->
       [n1, n2] = String.split(rest, ["(", ",", ")"], trim: true)
       sum + stoi(n1) * stoi(n2)
     end)
@@ -19,20 +19,20 @@ defmodule Advent.Day.Three do
   def part2 do
     input = "#{__DIR__}/input.test2" |> parse_input!() |> hd()
 
-    Regex.scan(~r/mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/, input)
-    |> List.flatten()
+    ~r/mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/
+    |> Regex.scan(input)
     |> Enum.reduce({0, true}, fn
-      "mul" <> rest, {sum, true} ->
+      ["mul" <> rest], {sum, true} ->
         [n1, n2] = String.split(rest, ["(", ",", ")"], trim: true)
         {sum + stoi(n1) * stoi(n2), true}
 
-      "mul" <> _rest, acc ->
+      ["mul" <> _rest], acc ->
         acc
 
-      "do()", {sum, _} ->
+      ["do()"], {sum, _} ->
         {sum, true}
 
-      "don't()", {sum, _} ->
+      ["don't()"], {sum, _} ->
         {sum, false}
     end)
     |> elem(0)
